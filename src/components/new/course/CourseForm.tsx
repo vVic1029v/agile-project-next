593877f228d3f6b933682d1ec5d1c4a4f4939686
Alexandra Ghita <anda.ghita05@gmail.com>
2025-02-25 19:34:52 +0200

    ceva

--- src/components/new/course/CourseForm.tsx
+++ src/components/new/course/CourseForm.tsx
@@ -13,24 +13,26 @@ const CourseForm: React.FC = () => {
   return (
     <Form className="p-6" action={(formData: FormData ) => {formData.append("homeClassId", selectedHomeClass?.id || ""); NewCourse(formData) }}>
 
-      {/* Selected Home Class Display */}
-      {selectedHomeClass ? (
-        <div className="mb-4 p-2 bg-blue-50 border border-blue-200 rounded-md">
-          <p className="text-sm text-blue-800">Selected Class: {selectedHomeClass.name}</p>
-        </div>
-      ) : (
-        <p className="text-sm text-gray-500 mb-4">No class selected.</p>
-      )}
 
-      {/* Button to Open Modal */}
+      <span className="flex">
+ 
       <button
         type="button"
-        className="px-4 py-2 bg-blue-600 text-white rounded-md"
+        className="px-4 py-2 bg-blue-600 text-white rounded-md size-[5vh] w-[20vh] min-h-10 min-w-10 max-w-30 max-h-20"
         onClick={() => setIsModalOpen(true)}
       >
         Select Class
       </button>
+      {selectedHomeClass ? (
+        <div className="bg-blue-50 border border-blue-200 rounded-md text-center items-center justify-center ml-5 flex size-[5vh] w-[30vh] min-h-10 min-w-30 max-w-50 max-h-20">
+          <p className="texmb-t-sm text-blue-800 px-3">Selected Class: {selectedHomeClass.name}</p>
+        </div>
+      ) : (
+        <p className="text-sm text-gray-500 ml-5 mt-2">No class selected.</p>
+      )}
 
+
+        
       {/* Modal */}
       <ModalOverlay onClose={() => setIsModalOpen(false)} isOpen={isModalOpen} title={"Select a class"}>
         <SearchHomeClassModal
@@ -41,9 +43,11 @@ const CourseForm: React.FC = () => {
           }}
         />
       </ModalOverlay>
+      </span>
+     
       
       {/* Submit Button */}
-      <button type="submit" className="px-4 py-2 bg-green-600 text-white rounded-md">
+      <button type="submit" className="px-4 py-2 bg-green-600 text-white rounded-md mt-6 size-[5vh] w-[20vh] min-h-11 min-w-10 max-w-26 max-h-20">
         Create Course
       </button>
     </Form>
