0869240af437c120e7aa29a0b393b4f39eddd15a
vVic1029v <victoryvictor11029@gmail.com>
2025-03-16 20:08:07 +0200

    a

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,7 @@ const eslintConfig = [
       '@next/next/no-page-custom-font': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/allowObjectTypes': 'off',
     },
   }),
 ]
