2b365ffcca4a0e9ad295913ead2cd6ff41e2b5af
vVic1029v <victoryvictor11029@gmail.com>
2025-03-17 22:29:59 +0200

    da

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,9 @@
+import { getToken } from "next-auth/jwt";
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
   callbacks: {
-    authorized: ({ req }) => {
+    authorized: async ({ req }) => {
       // Log the request cookies for debugging
       console.log("Request Cookies:", req.cookies);
 
@@ -10,13 +11,20 @@ export default withAuth({
       //const token = await getToken({ req, secret: process.env.AUTH_SECRET, secureCookie: true, cookieName: 
       // process.env.NODE_ENV !== 'production' ? 'authjs.session-token' : '__Secure-authjs.session-token', });
 
-      let sessionToken;
-      if (process.env.NODE_ENV !== 'production') {
-        sessionToken = req.cookies.get("authjs.session-token");
-      }
-      else {
-        sessionToken = req.cookies.get("__Secure-authjs.session-token");
-      }
+      // let sessionToken;
+      // if (process.env.NODE_ENV !== 'production') {
+      //   sessionToken = req.cookies.get("next-auth.session-token");
+      // }
+      // else {
+      //   sessionToken = req.cookies.get("__Secure-authjs.session-token");
+      // }
+
+      const secret = process.env.NEXT_AUTH_SECRET;
+      const sessionToken = await getToken({
+        req: req,
+        secret: secret,
+        raw: true,
+      });
 
       console.log("Session Token:", sessionToken); // Log the session token for debugging
 
