ee71c05a38a4d22d4ca23d2f167a443dc3ccb277
vVic1029v <victoryvictor11029@gmail.com>
2025-03-17 20:28:22 +0200

    test

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -31,10 +31,9 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
 
-  pages: {
-    signIn: "/auth/signin",
-    
-  },
+  // pages: {
+  //   signIn: "/auth/signin",
+  // },
 
 
   session: {
