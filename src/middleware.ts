be3c7360d95dbb5b196c866f2c39f83a564b337c
vVic1029v <victoryvictor11029@gmail.com>
2025-03-17 22:07:37 +0200

    sessionToken for production

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,12 @@
+import { getToken } from "next-auth/jwt";
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
   callbacks: {
-    authorized: ({ req }) => {
+    authorized: async ({ req }) => {
       // verify token and return a boolean
-      const sessionToken = req.cookies.get("next-auth.session-token");
+      // const sessionToken = req.cookies.get("next-auth.session-token");
+      const sessionToken = await getToken({ req, secret: process.env.AUTH_SECRET, secureCookie: true, cookieName: process.env.NODE_ENV !== 'production' ? 'authjs.session-token' : '__Secure-authjs.session-token', });
       if (sessionToken) return true;
       else return false;
     },
